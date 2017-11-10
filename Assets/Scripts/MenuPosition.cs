4c3f2d26899490bf9d3942cb95d2e51e08a2c837
mmahamat <michael.mahamat@free.fr>
2017-11-10 15:32:15 +0100

    Script position menu
    Ecriture du script pour la position du menu.

---
+++ Assets/Scripts/MenuPosition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPosition : MonoBehaviour {
+	//Script pour la position du menu par rapport au joueur, il ne devra pas bouger, et rester à distance fixe.
+	public GameObject player;
+	private Vector3 offset_menu;
+	// Use this for initialization
+	void Start () {
+		offset_menu = transform.position - player.transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = offset_menu + player.transform.position;
+	}
+}
